a9d34cc844750be1cc567369d00a031dff6cc593
patrick lin <patrickxlin@gmail.com>
2025-09-26 10:24:32 +0800

    chang tb.v power wire

--- test/tb.v
+++ test/tb.v
@@ -17,9 +17,9 @@ module tb ();
     end
 
     // wire up the inputs and outputs
-    reg  clk;
-    reg  rst_n;
-    reg  ena;
+    reg        clk;
+    reg        rst_n;
+    reg        ena;
     reg  [7:0] ui_in;
 
     wire [7:0] uo_out;
@@ -28,22 +28,28 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_patrick_lin_git_mcht_trx  user_project (
-  //tt_um_patrick_lin_git_mcht_trx  u_tt_um_patrick_lin_git_mcht_trx_0 (
-                                                                       // include power ports for the Gate Level test
+`ifdef GL_TEST
+    wire       VPWR = 1'b1;
+    wire       VGND = 1'b0;
+`endif
+
+
+  //tt_um_patrick_lin_git_mcht_trx  user_project (
+    tt_um_patrick_lin_git_mcht_trx  u_tt_um_patrick_lin_git_mcht_trx_0 (
+                                                                        // include power ports for the Gate Level test
     `ifdef GL_TEST
-                                                                       .VPWR    ( 1'b1 ),
-                                                                       .VGND    ( 1'b0 ),
+                                                                        .VPWR    ( VPWR ),
+                                                                        .VGND    ( VGND ),
     `endif
-                                                                       .ui_in   ( ui_in ),    // Dedicated inputs
-                                                                       .uo_out  ( uo_out ),   // Dedicated outputs
-                                                                       .uio_in  ( uio_in ),   // IOs: Input path
-                                                                       .uio_out ( uio_out ),  // IOs: Output path
-                                                                       .uio_oe  ( uio_oe ),   // IOs: Enable path (active high: 0=input, 1=output)
+                                                                        .ui_in   ( ui_in ),    // Dedicated inputs
+                                                                        .uo_out  ( uo_out ),   // Dedicated outputs
+                                                                        .uio_in  ( uio_in ),   // IOs: Input path
+                                                                        .uio_out ( uio_out ),  // IOs: Output path
+                                                                        .uio_oe  ( uio_oe ),   // IOs: Enable path (active high: 0=input, 1=output)
 
-                                                                       .ena     ( ena ),      // enable - goes high when design is selected
-                                                                       .clk     ( clk ),      // clock
-                                                                       .rst_n   ( rst_n )     // not reset
-                                                                      );
+                                                                        .ena     ( ena ),      // enable - goes high when design is selected
+                                                                        .clk     ( clk ),      // clock
+                                                                        .rst_n   ( rst_n )     // not reset
+                                                                       );
 
-endmodule
+endmodule // tb
